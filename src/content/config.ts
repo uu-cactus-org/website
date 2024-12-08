3faba0f424e68d014e65d54e6549091b9d8503c6
triort / tk <31843890+triort@users.noreply.github.com>
2024-12-08 14:52:46 +0900

    add: some files

---
+++ src/content/config.ts
@@ -0,0 +1,17 @@
+import { z, defineCollection } from 'astro:content';
+const blogCollection = defineCollection({
+    type: 'content',
+    schema: z.object({
+        title: z.string(),
+        pubdate: z.date(),
+        authers: z.array(z.string()),
+        categories: z.string(),
+        tags: z.array(z.string()),
+        coverImage: z.string(),
+        draft: z.boolean(),
+    }),
+});
+
+export const collections = {
+    'blog': blogCollection,
+};
