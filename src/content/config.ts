d91cde8864ff5c3d7851b11268fbcd71dfc22b96
hqru2637 <62416195+hqru2637@users.noreply.github.com>
2025-03-01 16:36:42 +0900

    feat: Paginationコンポーネントを実装

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,41 +1,45 @@
 import { z, defineCollection } from 'astro:content';
-const blogCollection = defineCollection({
-    type: 'content',
-    schema: z.object({
-        title: z.string(),
-        pubdate: z.date(),
-        authors: z.array(z.string()),
-        categories: z.string(),
-        tags: z.array(z.string()),
-        coverImage: z.string(),
-        draft: z.boolean(),
-    }),
-});
 
-const authors = defineCollection({
-    type: 'data',
-    schema: z.object({
-        id: z.string(),
-        name: z.string(),
-        bio: z.string(),
-        social: z.object({
-            twitter: z.optional(z.string()),
-            github:  z.optional(z.string()),
-            website: z.optional(z.string().url()),
-            custom: z.optional(
-                z.object({
-                    label: z.string(),
-                    url: z.string().url(),
-                    icon: z.string(),
-                }).array().max(3)
-            )
-        }),
-        status: z.string(),
-        profileImage: z.optional(z.string()).default('/src/assets/logos/cactus/cactakun_noimage.webp'),
+const blogSchema = z.object({
+    title: z.string(),
+    pubdate: z.date(),
+    authors: z.array(z.string()),
+    categories: z.string(),
+    tags: z.array(z.string()),
+    coverImage: z.string(),
+    draft: z.boolean(),
+});
+export type BlogSchema = z.infer<typeof blogSchema>;
+
+const authorSchema = z.object({
+    id: z.string(),
+    name: z.string(),
+    bio: z.string(),
+    social: z.object({
+        twitter: z.optional(z.string()),
+        github:  z.optional(z.string()),
+        website: z.optional(z.string().url()),
+        custom: z.optional(
+            z.object({
+                label: z.string(),
+                url: z.string().url(),
+                icon: z.string(),
+            }).array().max(3)
+        )
     }),
-})
+    status: z.string(),
+    profileImage: z.optional(z.string()).default('/src/assets/logos/cactus/cactakun_noimage.webp'),
+});
+export type AuthorSchema = z.infer<typeof authorSchema>;
 
 export const collections = {
-    'blog': blogCollection,
-    'authors': authors,
+    'blog': defineCollection({
+        type: 'content',
+        schema: blogSchema,
+    }),
+
+    'authors': defineCollection({
+        type: 'data',
+        schema: authorSchema,
+    }),
 };
