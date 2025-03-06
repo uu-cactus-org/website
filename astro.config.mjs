b2d8a7ee9da9a2a1dc4da9bae44d2bb68b5ca8f4
hqru2637 <62416195+hqru2637@users.noreply.github.com>
2025-03-06 14:01:03 +0900

    deps: add react integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
+import react from '@astrojs/react';
 
 // import updateArticle from './scripts/remark-ruby';
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [mdx()],
+    integrations: [mdx(), react()],
     legacy: {
         collections: true
     },
