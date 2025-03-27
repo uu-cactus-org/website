2b6e12a6a3bf4b4a011fb05aa556fe2fe427d366
hqru2637 <62416195+hqru2637@users.noreply.github.com>
2025-03-28 03:24:16 +0900

    refactor: テーマカラーをcss変数で定義+共通化

--- src/components/features/header/Hamburger.tsx
+++ src/components/features/header/Hamburger.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 
+import { variables } from "@styles/variables";
 import styles from "./hamburger.module.css";
 import hanburgerIcon from '@assets/icons/list.svg';
 import closeIcon from '@assets/icons/close.svg';
@@ -7,6 +8,10 @@ import closeIcon from '@assets/icons/close.svg';
 interface HamburgerProps {
   links: { href: string; label: string }[];
 }
+
+// convert rgba
+const bgOpacity = 0.98;
+const bgColor = `rgba(${variables.cactus.replace(/rgb\((\d+), (\d+), (\d+)\)/, '$1,$2,$3')}, ${bgOpacity})`;
 
 export default function Hamburger({ links }: HamburgerProps) {
   const [isOpen, setIsOpen] = useState(false);
@@ -25,7 +30,11 @@ export default function Hamburger({ links }: HamburgerProps) {
         }
       </button>
 
-      <div className={`${styles.background} ${isOpen ? styles.open : ''}`}>
+      <div
+        className={`${styles.background}
+        ${isOpen ? styles.open : ''}`}
+        style={{ backgroundColor: bgColor }}
+      >
         <div className={styles.container}>
           <hr />
           <nav>
---
+++ src/styles/variables.ts
@@ -0,0 +1,4 @@
+export const variables = {
+  'cactus': 'rgb(70, 189, 76)',
+  'cactus-dark': 'rgb(67, 184, 73)',
+} satisfies Record<string, string>;
