791c638c075c239263bf6aa6713e4351fe31dd9e
hqru2637 <62416195+hqru2637@users.noreply.github.com>
2025-09-01 18:32:28 +0900

    feat(ui): スタイルの大幅刷新

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,9 @@
 export const cactusTwitterUrl = 'https://x.com/uu_cactus';
 
 export const cactusGitHubUrl = 'https://github.com/uu-cactus-org';
+
+export const links: { href: string; label: string }[] = [
+    { href: "/about", label: "ABOUT" },
+    { href: "/blog", label: "BLOG" },
+    { href: "/game", label: "GAME" },
+];
